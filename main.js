2eb444c2f23933da6830d85ee4c436c185bd4da2
ch264 <christinavhastenrath@gmail.com>
2019-04-27 13:16:10 -0700

    convert celcius to kelving and fahrenheit function

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 // store iput in celsius div in variable
-const celciusInput = document.querySelector(".celsius > input");
-const fahrenheitInput = document.querySelector(".fahrenheit > input");
-const kelvinInput = document.querySelector(".kelvin > input");
+let celciusInput = document.querySelector(".celsius > input");
+let fahrenheitInput = document.querySelector(".fahrenheit > input");
+let kelvinInput = document.querySelector(".kelvin > input");
 
-// add event handler to input fields
+// add event handlers to input fields
 celciusInput.addEventListener('input', function() {
+	// grab the input value, convert to float number (decimal number)
+	let cTemp = parseFloat(celciusInput.nodeValue);
+	// convert temperatures
+	let fTemp = (cTemp * (9/5)) + 32;
+	let kTemp = cTemp + 273.15;
+	// show converted temperatures
+	fahrenheitInput.value = fTemp;
+	kelvinInput.value = kTemp;
+	})
+
+fahrenheitInput.addEventListener('input', function() {
 
 })
-fahrenheitInput.addEventListener('clicinput', function() {
-
-})
-kelvinInput.addEventListener('clicinput', function() {
+kelvinInput.addEventListener('input', function() {
 
 })
 
