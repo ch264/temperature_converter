5c9bb646c31c9a30773e51fa311077b00750b55f
ch264 <christinavhastenrath@gmail.com>
2019-04-27 13:06:55 -0700

    add event handlers to input fields

---
+++ main.js
@@ -0,0 +1,19 @@
+// store iput in celsius div in variable
+const celciusInput = document.querySelector(".celsius > input");
+const fahrenheitInput = document.querySelector(".fahrenheit > input");
+const kelvinInput = document.querySelector(".kelvin > input");
+
+// add event handler to input fields
+celciusInput.addEventListener('input', function() {
+
+})
+fahrenheitInput.addEventListener('clicinput', function() {
+
+})
+kelvinInput.addEventListener('clicinput', function() {
+
+})
+
+
+
+console.log(celciusInput);
